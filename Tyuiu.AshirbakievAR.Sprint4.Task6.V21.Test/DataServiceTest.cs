db612f317fcd34c3cbafe38f82571f93d948a01b
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-11-09 14:13:01 +0500

    Task6

---
+++ Tyuiu.AshirbakievAR.Sprint4.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AshirbakievAR.Sprint4.Task6.V21.Lib;
+
+namespace Tyuiu.AshirbakievAR.Sprint4.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var week = new string[] { "Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit" };
+            Assert.AreEqual(2, ds.Calculate(week));
+        }
+    }
+}
---
+++ Tyuiu.AshirbakievAR.Sprint4.Task6.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AshirbakievAR.Sprint4.Task6.V21.Lib;
+
+namespace Tyuiu.AshirbakievAR.Sprint4.Task6.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнил: Аширбакиев А. Р. | СмартБ 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Аширбакиев Алмаз Ринатович | СмартБ 23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных используя класс Array подсчитайте           *");
+            Console.WriteLine("* количество элементов, длина которых меньше 8.                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            var week = new string[] { "Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit" };
+            foreach(string c in week)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(week));
+            Console.ReadKey();
+        }
+    }
+}
